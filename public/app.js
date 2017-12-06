7859c89eb9a4135e566def15193badbfe7e86e2f
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-06 16:10:56 +0000

    Added a picture function

--- public/app.js
+++ public/app.js
@@ -8,24 +8,56 @@ var makeRequest = function(url, callback){
   request.send();
 }
 
-var populateList = function(beers){
-
-  var select = document.getElementById('beer-list');
-  beers.forEach(function(beer, index){
-
-    var option = document.createElement('option');
-    option.innerText = beer.name;
-    option.value = index;
-    select.appendChild(option);
-  });
-
-}
-
 var requestComplete = function(){
   if (this.status !== 200) return;
   var jsonString = this.responseText;
   var beers = JSON.parse(jsonString);
   populateList(beers);
+}
+
+// Beer Select populator
+
+var populateList = function(beers){
+  beers.forEach(function(beer, index){
+    this.createElementAndSetContent('beer-list', beer.name, index, 'option');
+  });
+
+  var dropDown = document.querySelector('select');
+
+  dropDown.addEventListener("change", function(){
+    var selectedBeer = beers[this.value]
+    beerInfo(selectedBeer);
+  });
+}
+
+var beerInfo = function(beer){
+  this.setElementAndSetContent('beer-details', beer.name, 0, 'beer-name')
+  this.setImage('beer-details', "<img src=" + beer.image_url + " style='height: 250px'>", 0, 'beer-image')
+};
+
+// DRY catch-all element creator and setter
+var createElementAndSetContent = function(parentElement, content, index, elementType){
+  var parent = document.getElementById(parentElement);
+  var element = document.createElement(elementType);
+  element.innerText = content;
+  element.value = index;
+  parent.appendChild(element);
+}
+
+var setElementAndSetContent = function(parentElement, content, index, elementId){
+  var parent = document.getElementById(parentElement);
+  var element = document.getElementById(elementId);
+  element.innerText = content;
+  element.value = index;
+  parent.appendChild(element);
+}
+
+var setImage = function(parentElement, content, index, elementId){
+  var parent = document.getElementById(parentElement);
+  var element = document.getElementById(elementId);
+  element.innerHTML = content;
+  element.value = index;
+  parent.appendChild(element);
 }
 
 var app = function(){
